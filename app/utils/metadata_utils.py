76840c541a256b32eaf65d40626ff2fbbf6fc032
drishya22 <drishyagarg22@gmail.com>
2026-07-09 22:54:31 +0530

    Implement metadata storage and duplicate image detection

---
+++ app/utils/metadata_utils.py
@@ -0,0 +1,14 @@
+import json
+import os 
+METADATA_FILE="data/image_metadata.json"
+
+def load_metadata():
+    if not os.path.exists(METADATA_FILE):
+        return []
+    
+    with open(METADATA_FILE,"r") as file:
+        return json.load(file)
+    
+def save_metadata(data):
+    with open(METADATA_FILE,"w") as file:
+        json.dump(data,file,indent=4)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI,UploadFile,File,HTTPException
 import os
+from app.utils.hash_utils import generate_hash
+from app.utils.metadata_utils import load_metadata,save_metadata 
 
 app=FastAPI(title="Photo Management Platform")
 
@@ -22,10 +24,31 @@ async def upload_image(file: UploadFile=File(...)):
             status_code=400,
             detail="Only JPG,JPEG and PNG files are allowed"
         )
+    file_bytes=await file.read()
+    image_hash=generate_hash(file_bytes)
+    metadata=load_metadata()
+    for image in metadata:
+        if image["hash"]==image_hash:
+            return {
+                "is_duplicate":True,
+                "message":"Duplicate image detected",
+                "existing_filename":image["filename"],
+                "hash":image_hash
+            }
     file_path=os.path.join(UPLOAD_FOLDER,file.filename)
     with open(file_path,"wb") as buffer:
-        buffer.write(await file.read())
-    return {
+        buffer.write(file_bytes)
+    
+    metadata.append({
         "filename":file.filename,
-        "saved_to":file_path
+        "hash":image_hash,
+        "file_type":extension
+    })
+    save_metadata(metadata)
+    return {
+        "is_duplicate": False,
+        "filename":file.filename,
+        "saved_to":file_path,
+        "file_type": extension,
+        "hash":image_hash
     }
